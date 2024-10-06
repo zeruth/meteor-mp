a0bd6186d0a45af60a660b0365210b7392b16240
Tyler Bochard <zeruth09@gmail.com>
2024-10-05 23:36:38 -0400

    target android api 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,12 +10,12 @@ plugins {
 
 android {
     namespace = "com.meteor.android"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.meteor.android"
         minSdk = 30
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 

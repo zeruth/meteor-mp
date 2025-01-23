0a26233ecea564df3d1e98b5e19bbc35609bdc8b
Tyler Bochard <zeruth09@gmail.com>
2025-01-23 06:13:48 -0500

    feat(droid): make keyboard button bottom

--- android/src/main/java/meteor/input/KeyboardButton.kt
+++ android/src/main/java/meteor/input/KeyboardButton.kt
@@ -6,7 +6,7 @@ import meteor.common.ui.components.sidebar.SidebarButton
 import meteor.input.KeyboardController.keyboardController
 import meteor.ui.GamePanel.gamePanelFocusRequester
 
-class KeyboardButton: SidebarButton(icon = LineAwesomeIcons.KeyboardSolid, actionButton = true) {
+class KeyboardButton: SidebarButton(icon = LineAwesomeIcons.KeyboardSolid, actionButton = true, bottom = true) {
     override fun onClick() {
         gamePanelFocusRequester.requestFocus()
         keyboardController.show()

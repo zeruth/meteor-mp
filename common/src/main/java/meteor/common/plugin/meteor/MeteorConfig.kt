f7ca5418af61e0c17e934a6abcd509d37413d90c
Tyler Bochard <zeruth09@gmail.com>
2025-02-12 23:08:31 -0500

    fix(common): remove example configs

--- common/src/main/java/meteor/common/plugin/meteor/MeteorConfig.kt
+++ common/src/main/java/meteor/common/plugin/meteor/MeteorConfig.kt
@@ -9,7 +9,4 @@ class MeteorConfig(plugin: MeteorPlugin) : Config(plugin) {
     val filterQuality = ConfigItem(this, "Filter quality", "filterQuality".key(),
         FilterQuality.None
     )
-    val test2 = ConfigItem(this, "1", "1".key(), false)
-    val test1 = ConfigItem(this, "2", "2".key(), "")
-    val testStr = ConfigItem(this, "3", "3".key(), 123321)
 }

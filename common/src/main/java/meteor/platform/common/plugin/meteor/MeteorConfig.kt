ceb47b3acabfa52767403dd054f382460ec91c96
Tyler Bochard <zeruth09@gmail.com>
2025-02-14 11:18:37 -0500

    feat(common): use white as default ui color

--- common/src/main/java/meteor/platform/common/plugin/meteor/MeteorConfig.kt
+++ common/src/main/java/meteor/platform/common/plugin/meteor/MeteorConfig.kt
@@ -5,7 +5,7 @@ import meteor.platform.common.config.ConfigItem
 import meteor.plugin.meteor.FilterQuality
 
 class MeteorConfig(plugin: MeteorPlugin) : Config(plugin) {
-    val uiColor = ConfigItem(this, "UI color", "uicolor".key(), UIColor.GREEN)
+    val uiColor = ConfigItem(this, "UI color", "uicolor".key(), UIColor.WHITE)
     val filterQuality = ConfigItem(this, "Filter quality", "filterQuality".key(),
         FilterQuality.None
     )
